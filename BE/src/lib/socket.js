6a8077b078c290da0f0dbf2223f8458e6c70847d
ZeynelBasaran <113614758+ZeynelBasaran@users.noreply.github.com>
2026-03-09 16:52:51 +0300

    Update socket.js

--- BE/src/lib/socket.js
+++ BE/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "https://chatappzb.netlify.app"],
+        origin: ["http://localhost:3000", "https://chatappzb.netlify.app","https://chat-app-lac-two-42.vercel.app"],
         credentials: true,
     },
 });
